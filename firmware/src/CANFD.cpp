c5f06a5efc5e8353735bf116614be11817b43578
midarubiccube <mi.dai.tomo@gmail.com>
2026-08-04 03:00:27 +0900

    MCP3208を読むプログラムを追加

--- firmware/src/CANFD.cpp
+++ firmware/src/CANFD.cpp
@@ -6,10 +6,10 @@
 #include "message.hpp"
 
 void CANFD::start(){
-	if (HAL_FDCAN_ConfigGlobalFilter(fdcan_, FDCAN_REJECT, FDCAN_REJECT, FDCAN_ACCEPT_IN_RX_FIFO0, FDCAN_ACCEPT_IN_RX_FIFO0) != HAL_OK)
+	/*if (HAL_FDCAN_ConfigGlobalFilter(fdcan_, FDCAN_REJECT, FDCAN_REJECT, FDCAN_ACCEPT_IN_RX_FIFO0, FDCAN_ACCEPT_IN_RX_FIFO0) != HAL_OK)
 	{
 		Error_Handler();
-	}
+	}*/
 	if(HAL_FDCAN_Start(fdcan_)!= HAL_OK) {
 		Error_Handler();
 	}
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -2,6 +2,8 @@
 #include <cstring>
 #include "stdio.h"
 
+#include "spi.h"
+
 #include "message.hpp"
 #include "CANFD.hpp"
 #include "FullColorLED.hpp"
@@ -11,6 +13,33 @@ FullColorLED led{&htim1, TIM_CHANNEL_1};
 int data;
 extern osTimerId_t dischargeTimerHandle;
 bool onoff = false;
+
+#define MCP3208_CS_LOW()  HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_RESET)
+#define MCP3208_CS_HIGH() HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_SET)
+
+#define MCP3208_START_BIT   0x04
+#define MCP3208_MODE_SINGLE 0x02
+#define MCP3208_MODE_DIFF   0x00
+
+uint16_t MCP3208_Read(uint8_t channel)
+{
+  uint8_t tx[3];
+  uint8_t rx[3];
+  MCP3208_CS_LOW();
+
+  tx[0] = MCP3208_START_BIT | MCP3208_MODE_SINGLE | ((channel & 0x04) >> 2);
+  tx[1] = (channel & 0x03) << 6;
+  tx[2] = 0;
+
+  HAL_SPI_TransmitReceive(&hspi1, tx, rx, 3, HAL_MAX_DELAY);
+
+  MCP3208_CS_HIGH();
+
+  uint16_t value;
+  value = ((rx[1] & 0x0F) << 8) | rx[2];
+
+  return value;
+}
 
 void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef *hfdcan, uint32_t RxFifo0ITs) {
   if((RxFifo0ITs & FDCAN_IT_RX_FIFO0_NEW_MESSAGE) != RESET) {
@@ -46,12 +75,11 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 
 extern "C" void StartDefaultTask(void *argument)
 {
-
   led.set_rgb(255, 0, 0);
   led.start();
   canfd = new CANFD(&hfdcan1);
 	canfd->start();
-  canfd->set_filter_mask(1048576, 0xFFC000);
+  //canfd->set_filter_mask(1048576, 0xFFC000);
   
 
 	CANFD_Frame test;
@@ -59,12 +87,13 @@ extern "C" void StartDefaultTask(void *argument)
 	test.size = 32;
 	memset(test.data, 0, 64);
 
-	//canfd->tx(test);
+	canfd->tx(test);
 
   float ad;
 
   while (1)
   {
+    printf("%d %d\n", MCP3208_Read(2), MCP3208_Read(1));
     if (canfd->rx_available())
     {
       CANFD_Frame data;
@@ -90,7 +119,7 @@ extern "C" void StartDefaultTask(void *argument)
     if( HAL_ADC_PollForConversion(&hadc1, 1000) == HAL_OK )
     {
 	      ad = (HAL_ADC_GetValue(&hadc1) - 350) / 62.0;
-        printf("test=%f\n", ad);
+        //printf("test=%f\n", ad);
     }
     HAL_ADC_Stop(&hadc1);
     osDelay(10);
